54c07dc45c91ea67b59a132dddf5e0b1019eefbf
rrogerr <rgcbll@gmail.com>
2016-11-06 14:08:42 -0600

    lol

--- plot6.R
+++ plot6.R
@@ -1,6 +1,9 @@
-########################## PLOT5 ##########################
-# How have emissions from motor vehicle sources changed 
-# from 1999–2008 in Baltimore City?
+########################## PLOT6 ##########################
+# Compare emissions from motor vehicle sources in Baltimore 
+# City with emissions from motor vehicle sources in Los 
+# Angeles County, California (fips == "06037"). Which city 
+# has seen greater changes over time in motor vehicle 
+# emissions?
 
 
 ######################### IMPORTS #########################
@@ -27,13 +30,17 @@ SCC_motor <- grepl("[Mm]otor [Vv]ehicle", SCC$Short.Name)
 # getting the codes to subset in NEI
 codes_motor <- SCC$SCC[SCC_motor]
 
-# subsetting and collapsing NEI
-NEI_motor <- filter(NEI, SCC %in% codes_motor & fips == "24510") %>% 
+# subsetting and collapsing NEI for Baltimore City
+NEI_BC <- filter(NEI, SCC %in% codes_motor & fips == "24510") %>% 
         aggregate(Emissions ~ year, data = ., sum)
 
 # IMPORTANT: It seems that there is no data available for 
 # motor vehicle sources in Baltimore City in years 1999
 # and 2008
+
+# subsetting and collapsing NEI for Los Angeles
+NEI_LA <- filter(NEI, SCC %in% codes_motor & fips == "06037") %>% 
+        aggregate(Emissions ~ year, data = ., sum)
 
 
 ####################### PLOT DEVICE #######################
@@ -42,15 +49,16 @@ ath <- "exploratory_data_analysis_final_project"
 path <- paste0(p, ath)
 setwd(path)
 
-png("./plot5.png")
-p <- ggplot(NEI_motor, aes(year, Emissions)) +
-        geom_point(col = "red", size = 3) +
+png("./plot6.png")
+p <- ggplot(NEI_LA, aes(year, Emissions)) +
+        geom_point(aes(color = "Los Angeles"), size = 3) +
         geom_smooth(method = "lm") +
-        labs(title = "emissions from motor vehicle sources in Baltimore City") +
+        geom_point(data = NEI_BC, aes(color = "Baltimore City"), size = 3) +
+        geom_smooth(data = NEI_BC, method = "lm") +
+        labs(title = "emissions from motor vehicle sources\n(Baltimore City vs. Los Angeles)") +
         scale_x_discrete(limits = c(1999, 2002, 2005, 2008)) +
         labs(y = "Emissions (tons)") +
         theme_set(theme_gray(base_size = 14)) +
-        theme(plot.margin = unit(c(1.5,1.5,1.5,0.5), "lines"))
+        theme(plot.margin = unit(c(2.5,1.5,1.5,0.5), "lines"))
 print(p)
 dev.off()
-
